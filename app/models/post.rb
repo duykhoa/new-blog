32866f99c46ae4c13c33fb4f1443c54edc4f2226
Duy Khoa <duykhoa12t@gmail.com>
2016-02-15 00:51:37 +0800

    Correct typo and reduce number of related_posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  RELATED_POST_MAX = 10
+  RELATED_POST_MAX = 5
 
   def self.related_with(id)
     Post.where("id != ?", id).limit(RELATED_POST_MAX)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :posts
-    resource :related_posts, only: %i(show)
+    resources :related_posts, only: %i(show)
   end
 end
