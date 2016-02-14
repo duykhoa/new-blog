41ab97ffc794d0159fe7dc2c83ee90bc0f9beed3
Duy Khoa <duykhoa12t@gmail.com>
2016-02-15 00:07:19 +0800

    Add show, index action [WIP]

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,8 +1,14 @@
 class Api::PostsController < ApplicationApiController
+  # GET /api/posts
+  #
+  # Returns a list of posts
   def index
     respond_with Post.all
   end
 
+  # GET /api/posts/:id
+  #
+  # Returns a post
   def show
     respond_with Post.find_by_id(params[:id])
   end
