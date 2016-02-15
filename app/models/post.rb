21ee8ac861578331e170e5785a29a1d5e046f179
Duy Khoa <duykhoa12t@gmail.com>
2016-02-16 00:57:28 +0800

    Implement category search

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,27 @@ class Post < ActiveRecord::Base
 
   # Public: Get related post with post id
   #
+  #   id - The integer represents post id
+  #
   # Examples
   #   Post.related_with(Post.first.id)
   #   # => [...]
   #
-  # Returns a list of related posts
+  # Returns a list of posts
   def self.related_with(id)
     Post.where("id != ?", id).limit(RELATED_POST_MAX)
+  end
+
+  # Public: Return a list of posts that have category in the arg
+  #
+  #   category - String category
+  #
+  # Examples
+  #   Post.by_category("Travel and working")
+  #   # => [...]
+  #
+  # Returns a list of posts
+  def self.by_category(category)
+    Post.where("category = ?", category)
   end
 end
