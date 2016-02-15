2ca5973f6b957f35bb76156d12773a5adada3622
Duy Khoa <duykhoa12t@gmail.com>
2016-02-16 01:04:57 +0800

    Use lazy attribute in factory of post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :post do
-    title Faker::Lorem.sentence
-    content Faker::Lorem.paragraph(2)
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph(2) }
+    category { CATEGORY.sample }
   end
 end
