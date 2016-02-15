4caa43991c425413f0ca241007083ab21d4eecfd
Duy Khoa <duykhoa12t@gmail.com>
2016-02-16 00:21:28 +0800

    Add doc for Post#related_with :wink:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ActiveRecord::Base
   RELATED_POST_MAX = 5
 
+  # Public: Get related post with post id
+  #
+  # Examples
+  #   Post.related_with(Post.first.id)
+  #   # => [...]
+  #
+  # Returns a list of related posts
   def self.related_with(id)
     Post.where("id != ?", id).limit(RELATED_POST_MAX)
   end
