41e4501f9cc18fd83a64f9c3ff85e4bcff2cb071
Duy Khoa <duykhoa12t@gmail.com>
2016-02-21 21:10:24 +0800

    Config gulpfile to build the element

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var del= require('del');
+var minifyHTML = require('gulp-minify-html');
+var runSequence = require('run-sequence');
+
+gulp.task('vulcanize', function () {
+  return gulp.src([
+      'public/elements/cara-home-page.html',
+    ])
+    .pipe(vulcanize({
+    }))
+    .pipe(gulp.dest('public/dist/vulcanize'));
+});
+
+gulp.task('clean', function () {
+  return del([
+    'public/dist/vulcanize/*',
+  ])
+});
+
+gulp.task('minify', function() {
+  var opts = {
+    collapseWhitespace: true,
+    removeComments: true,
+    quoteCharacter: '\'',
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: true
+  };
+
+  return gulp.src('public/dist/vulcanize/*')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'vulcanize',
+    'minify',
+    'clean'
+  );
+});
+
+gulp.task('default', ['build']);
