cbb6e6826fda34c9f5664df0c239c85cd577045a
Duy Khoa <duykhoa12t@gmail.com>
2016-04-09 14:10:11 +0800

    temporary map the posts data

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,22 @@
 class Admin::PostsController < ApplicationAdminController
   def index
+    @posts = to_json(Post.all)
+  end
+
+  private
+
+  def to_json(posts)
+    JSON.generate(posts.map { |post| jsonizable(post) })
+  end
+
+  def jsonizable(post)
+    {
+      title: post.title,
+      short_content: post.short_content,
+      category: post.category,
+      editUrl: '/',
+      deleteUrl: '/',
+      viewUrl: '/'
+    }
   end
 end
