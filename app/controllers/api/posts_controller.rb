6ecf3712892eaf98fa5f07d75ded356517fc8928
Duy Khoa <duykhoa12t@gmail.com>
2016-05-07 20:34:18 +0800

    Fix authenticate problem for show api

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::PostsController < ApplicationApiController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /api/posts
   #
   # Returns a list of posts
